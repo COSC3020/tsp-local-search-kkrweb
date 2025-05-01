28f174c86e6034b6f68beb65758c8a60b27f2259
Kane Kriz <kanemkriz@gmail.com>
2025-05-01 07:15:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,165 @@
 //
 
 
+function tsp_ls(cityDistanceMatrix) 
+{
+    if((cityDistanceMatrix == null) || (cityDistanceMatrix.length == 0)) 
+    {
+        return -1;
+    }
 
+    if(cityDistanceMatrix.length == 1) 
+    {
+        return 0;
+    }
+
+ //
+
+    var numCities = cityDistanceMatrix.length;
+    var currentPath = [];
+    var newPath = [];
+    
+    var shortestDistance = null;
+    var currentDistance = null;
+    var newDistance = null;
+    
+    var repeatCount = 0;
+    var totalIterations = 0;
+
+    
+ //
+
+    
+    for(var i = 0; i < numCities; i++) 
+    {
+        currentPath[i] = i;
+    }
+
+    for(var i = numCities - 1; i > 0; i--) 
+    {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = currentPath[i];
+        
+        currentPath[i] = currentPath[j];
+        currentPath[j] = temp;
+    }
+
+    currentDistance = 0;
+    
+    for(var i = 0; i < numCities - 1; i++) 
+    {
+        var prevCity = currentPath[i];
+        var currentCity = currentPath[i + 1];
+        
+        if(cityDistanceMatrix[prevCity][currentCity] < 0) 
+        {
+            currentDistance = null;
+            break;
+        }
+        
+        currentDistance += cityDistanceMatrix[prevCity][currentCity];
+    }
+    shortestDistance = currentDistance;
+
+    while(true) 
+    {
+        totalIterations++;
+
+        var i = Math.floor(Math.random() * (numCities - 1));
+        var k = i + 1 + Math.floor(Math.random() * (numCities - i - 1));
+
+        for(var j = 0; j < i; j++) 
+        {
+            newPath[j] = currentPath[j];
+        }
+        
+        for(var j = i; j <= k; j++) 
+        {
+            newPath[j] = currentPath[k - (j - i)];
+        }
+        
+        for(var j = k + 1; j < numCities; j++) 
+        {
+            newPath[j] = currentPath[j];
+        }
+
+        newDistance = 0;
+        
+        for(var j = 0; j < numCities - 1; j++) 
+        {
+            var prevCity = newPath[j];
+            var currentCity = newPath[j + 1];
+            
+            if(cityDistanceMatrix[prevCity][currentCity] < 0) 
+            {
+                newDistance = null;
+                break;
+            }
+            
+            newDistance += cityDistanceMatrix[prevCity][currentCity];
+        }
+
+        if(newDistance != null) 
+        {
+            if(currentDistance == null) 
+            {
+                for(var j = 0; j < numCities; j++) 
+                {
+                    currentPath[j] = newPath[j];
+                }
+                
+                currentDistance = newDistance;
+                shortestDistance = newDistance;
+                repeatCount = 0;
+            }
+                
+            else if(newDistance < currentDistance) 
+            {
+                for(var j = 0; j < numCities; j++) 
+                {
+                    currentPath[j] = newPath[j];
+                }
+                
+                currentDistance = newDistance;
+                
+                if(shortestDistance == null || (newDistance < shortestDistance)) 
+                {
+                    shortestDistance = newDistance;
+                }
+                
+                repeatCount = 0;
+            }
+                
+            else 
+            {
+                repeatCount++;
+            }
+        }
+            
+        else 
+        {
+            repeatCount++;
+        }
+    }
+
+       if(shortestDistance != null)
+       {
+        return shortestDistance;
+        } 
+           
+       else 
+       {
+        return -1;
+        }
+}
+
+
+
+////
+
+
+
+/*
 
 function tsp_ls(cityDistanceMatrix) 
 {
@@ -165,4 +323,4 @@ function tsp_ls(cityDistanceMatrix)
 }
 
 
-//
+*/
